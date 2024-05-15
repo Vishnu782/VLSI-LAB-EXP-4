e07bf71c7717407b48fb2dd6cacb45a9d34b86ef
VISHNU V <102226356+Vishnu782@users.noreply.github.com>
2024-05-15 08:01:26 +0530

    Update Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,8 +1,21 @@
-6(ii): JK FLIP FLOP 
-module jk( j,k,clk,q); 
-input j,k,clk; 
-output reg q=0; 
+6(iii): D FLIP FLOP 
+module dff( clk,d,q); 
+input clk,d; 
+output reg q; 
 
 
 
+endmodule
+module Dflipflop(D,clk,reset,Q);
+input D;
+input clk;
+input reset;
+output reg Q;
+always @(posedge clk)
+begin
+ if(reset==1'b1)
+  Q <= 1'b0;
+ else
+  Q <= D;
+end
 endmodule
